97a6aedbfae49b756445ccbb11696ec5b8869ad2
Aiden <aidenbak9@gmail.com>
2026-04-17 10:17:01 -0500

    Initial Commit

---
+++ finalproject2/finalproject2App.swift
@@ -0,0 +1,17 @@
+//
+//  finalproject2App.swift
+//  finalproject2
+//
+//  Created by Aiden Baker on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct finalproject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finalproject2Tests/finalproject2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  finalproject2Tests.swift
+//  finalproject2Tests
+//
+//  Created by Aiden Baker on 4/17/26.
+//
+
+import Testing
+@testable import finalproject2
+
+struct finalproject2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
